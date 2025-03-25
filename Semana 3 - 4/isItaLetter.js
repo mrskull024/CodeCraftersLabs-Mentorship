cacf3e79a17a5dbec963433c223c47c112e93357
Julio Martinez Castillo <30775269+mrskull024@users.noreply.github.com>
2025-03-24 20:43:17 -0600

    Se agrega la solucion correcta a la kata isItALetter

--- Semana 3 - 4/isItaLetter.js
+++ Semana 3 - 4/isItaLetter.js
@@ -1,8 +1,3 @@
 function isItLetter(character) {
-    const isLetter = typeof (character) === 'string' ? true : false;
-
-    if (isLetter)
-        return `'${character}' is a letter`;
-
-    return `'${character}' is not a letter`;
+    return /^[a-zA-Z]$/.test(character);
 }
